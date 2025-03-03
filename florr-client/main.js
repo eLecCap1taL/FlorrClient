8ae70979cd07028823fb7dc9be2cee03f27b4b8f
eLecCap1taL <166891662+eLecCap1taL@users.noreply.github.com>
2025-03-03 21:29:54 +0800

    upd 1.0

--- florr-client/main.js
+++ florr-client/main.js
@@ -8,13 +8,13 @@ app.whenReady().then(() => {
         width: 1280,
         height: 720,
         title: "Florr Client", // 设置窗口标题
-        icon: "https://florr.io/favicon.ico", // 设置窗口图标
-        // frame: false,
+        icon: path.join(__dirname, './icon.ico'), // 设置窗口图标
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
             nodeIntegration: false
         }
     });
 
+    mainWindow.setMenu(null);
     mainWindow.loadURL("https://florr.io");
 });
